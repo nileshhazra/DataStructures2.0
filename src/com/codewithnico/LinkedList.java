08daa70e370fbfa773b010953b5715f3695aa76f
nileshhazra <nileshhz99@gmail.com>
2020-09-29 19:53:19 +0530

    add methods

--- src/com/codewithnico/LinkedList.java
+++ src/com/codewithnico/LinkedList.java
@@ -1,8 +1,11 @@
 package com.codewithnico;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
     private Node first;
     private Node last;
+    private int size;
     private boolean isEmpty(){
         return first == null;
     }
@@ -13,27 +16,15 @@ public class LinkedList {
             this.value = value;
         }
     }
-    public  void addLast(int item) {
-        var node = new Node(item);
-        if (isEmpty())
-            first = last = node;
-        else {
-            last.next = node;
-            last = node;
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current!=null) {
+            if (current.next == node) return current;
+            current = current.next;
         }
-
-
+        return null;
     }
-    public void addFirst(int item) {
-        var node = new Node(item);
-        if (isEmpty())
-            first=last=node;
-        else{
-            node.next = first;
-            first = node;
-        }
 
-    }
     public int indexOf(int item){
         int index = 0;
         var current = first;
@@ -47,5 +38,69 @@ public class LinkedList {
     public boolean contains(int item) {
         return indexOf(item) != -1;
     }
+    public  void addLast(int item) {
+        var node = new Node(item);
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+        size++;
+
+
+    }
+    public void addFirst(int item) {
+        var node = new Node(item);
+        if (isEmpty())
+            first=last=node;
+        else{
+            node.next = first;
+            first = node;
+        }
+        size++;
+
+    }
+    public void removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first==last)
+            first=last=null;
+        else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+        size--;
+    }
+    public void removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last)
+            first = last = null;
+        else {
+            var previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+        size--;
+    }
+    public int size(){
+        return size;
+    }
+    public int[] toArray() {
+        int[] array = new int[size];
+        var current = first;
+        var index = 0;
+        while (current!=null){
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
+
 
 }
--- src/com/codewithnico/Main.java
+++ src/com/codewithnico/Main.java
@@ -1,14 +1,22 @@
 package com.codewithnico;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         var List = new LinkedList();
+        System.out.println(List.size());
         List.addLast(10);
         List.addLast(20);
         List.addLast(30);
         List.addFirst(5);
+        List.removeLast();
+        List.removeFirst();
+        System.out.println(List.size());
+        var array = List.toArray();
+        System.out.println(Arrays.toString(array));
+
 
         System.out.println(List.contains(20));
 
