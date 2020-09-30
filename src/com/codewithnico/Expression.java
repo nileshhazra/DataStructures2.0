1438e97547145b787e114d225b1362f3ba5196bc
nileshhazra <nileshhz99@gmail.com>
2020-10-01 02:53:12 +0530

    isBalanced method

---
+++ src/com/codewithnico/Expression.java
@@ -0,0 +1,18 @@
+package com.codewithnico;
+
+import java.util.Stack;
+
+public class Expression {
+    public boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch: input.toCharArray()) {
+            if (ch =='(')
+                stack.push(ch);
+            if (ch == ')')
+                stack.pop();
+            
+        }
+        return stack.empty();
+    }
+}
--- src/com/codewithnico/Main.java
+++ src/com/codewithnico/Main.java
@@ -6,9 +6,9 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "ENOLA ";
-        StringReverser reverser = new StringReverser();
-        var result = reverser.reverse(str);
+       String str = "(1 + 2";
+       Expression exp = new Expression();
+       var result = exp.isBalanced(str);
         System.out.println(result);
 
 
